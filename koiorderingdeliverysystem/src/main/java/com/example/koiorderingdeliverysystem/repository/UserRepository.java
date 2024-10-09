134f489de4fd99065f823400905a2dbca0d56b37
VuJason <88337087+VuJason@users.noreply.github.com>
2024-10-09 18:13:55 +0700

    Delete koiorderingdeliverysystem directory

--- koiorderingdeliverysystem/src/main/java/com/example/koiorderingdeliverysystem/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.koiorderingdeliverysystem.repository;
-import com.example.koiorderingdeliverysystem.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
