474cbfde5700b90c200c99f6eafffe6ccea8a094
VuJason <88337087+VuJason@users.noreply.github.com>
2024-10-20 20:15:30 +0700

    Delete koiorderingdeliverysystem directory

--- koiorderingdeliverysystem/src/main/java/com/example/koiorderingdeliverysystem/repository/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.koiorderingdeliverysystem.repository;
-import com.example.koiorderingdeliverysystem.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrdersRepository extends JpaRepository<Orders, Integer> {
-//   Orders findByOrderId(int id);
-
-}
