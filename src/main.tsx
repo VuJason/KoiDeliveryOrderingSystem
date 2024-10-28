5d2f84647164d0d19916c8e459ddfdaff50ccc5b
bug-createdme <fairytail200347@gmail.com>
2024-10-28 22:56:27 +0700

    update pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import LoginPage from "./pages/LoginPage.tsx";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage.tsx";
-import VerifyOTPPage from "./pages/VerifyOTPPage.tsx";
-import ChangPasswordPage from "./pages/ChangePasswrod.tsx";
 import App from "./App.tsx";
-import TrackPage from "./pages/TrackPage.tsx";
-import BrowserTrack from  "./pages/BrowserTrack.tsx";
+import "./index.css";
+import BrowserTrack from "./pages/BrowserTrack.tsx";
+import ChangPasswordPage from "./pages/ChangePasswrod.tsx";
 import DeliveryTrackPage from "./pages/DeliveryTrackPage.tsx";
-import Account from "./pages/Account.tsx";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import TrackPage from "./pages/TrackPage.tsx";
+import VerifyOTPPage from "./pages/VerifyOTPPage.tsx";
 import { adminRoutes } from "./routes/AdminRoutes.tsx";
-
 
 const router = createBrowserRouter([
   {
@@ -47,11 +45,7 @@ const router = createBrowserRouter([
     path: "/delivery-track",
     element: <DeliveryTrackPage />,
   },
-  {
-    path: "/account",
-    element: <Account />
-  }
-...adminRoutes,
+  ...adminRoutes,
 ]);
 
 createRoot(document.getElementById("root")!).render(
