02d2ddd638ad81bb1dfa3866d494cb4c58cc6609
VuJason <minhtrong220703@gmail.com>
2024-11-21 21:25:51 +0700

    new upload

--- src/main/java/com/example/koiorderingdeliverysystem/repository/TransactionsRepository.java
+++ src/main/java/com/example/koiorderingdeliverysystem/repository/TransactionsRepository.java
@@ -2,6 +2,13 @@ package com.example.koiorderingdeliverysystem.repository;
 
 import com.example.koiorderingdeliverysystem.entity.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TransactionsRepository extends JpaRepository<Transactions, String> {
+    @Query("SELECT MONTH(t.transactionDate) AS month, SUM(t.amount) AS totalRevenue " +
+            "FROM Transactions t GROUP BY MONTH(t.transactionDate)")
+    List<Object[]> findMonthlyRevenue();
+
 }
--- src/main/java/com/example/koiorderingdeliverysystem/service/AdminService.java
+++ src/main/java/com/example/koiorderingdeliverysystem/service/AdminService.java
@@ -6,6 +6,7 @@ import com.example.koiorderingdeliverysystem.dto.response.UserResponse;
 import com.example.koiorderingdeliverysystem.entity.Roles;
 import com.example.koiorderingdeliverysystem.entity.Users;
 import com.example.koiorderingdeliverysystem.repository.OrdersRepository;
+import com.example.koiorderingdeliverysystem.repository.TransactionsRepository;
 import com.example.koiorderingdeliverysystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,9 @@ public class AdminService {
     @Autowired
     OrdersRepository ordersRepository;
 
+    @Autowired
+    TransactionsRepository transactionsRepository;
+
 
     public Map<String, Object> getDashboardStats() {
         Map<String, Object> stats = new HashMap<>();
@@ -39,6 +43,16 @@ public class AdminService {
         // số lượng staff
         long staffCount =  userRepository.countByRole(Roles.STAFF);
         stats.put("staffCount", staffCount);
+
+
+        List<Object[]> monthlyRevenue = transactionsRepository.findMonthlyRevenue();
+        Map<Integer, Double> revenueMap = new HashMap<>();
+        for (Object[] row : monthlyRevenue) {
+            int month = (Integer) row[0];
+            double totalRevenue = (Double) row[1];
+            revenueMap.put(month, totalRevenue);
+        }
+        stats.put("monthlyRevenue", revenueMap);
 
         return stats;
         //doanh thu từng tháng
